4ee1539a93ea435b22051a04d81a5f10b3ae841c
fetul_jv25 <fetul_jv25@STHQ0122-08>
2024-07-21 12:05:10 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleApp1;
+
+UserContex db = new UserContex();
+
+User user = new User() { Login = "Maga1090", Password = "12345" };
+
+db.Add(user);
+db.SaveChanges();
---
+++ ConsoleApp1/UserContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ConsoleApp1;
+
+public class UserContex:DbContext
+{
+    DbSet<User> Users { get; set; }
+    DbSet<Tags> Tags { get; set; }
+
+    DbSet<Posts> Posts { get; set; }
+
+    public UserContex()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=USERS;Integrated Security=SSPI;");
+    }
+}
